b123ed6568f98f6022734c65d7166fad4d0ba37b
IdeaBanana <pirate228kek@gmail.com>
2024-06-20 16:31:14 +0300

    1

---
+++ Resousrce/AbstractItem.gd
@@ -0,0 +1,29 @@
+extends Resource
+
+class_name AbstractItem
+
+@export var name: String
+@export var powerful: bool
+@export var rarely: bool
+@export var texture: Texture2D
+
+func get_item_name() -> String:
+	return name
+
+func get_powerful() -> bool:
+	return powerful
+
+func get_rarely() -> bool:
+	return rarely
+
+func get_texture() -> Texture2D:
+	if !texture:
+		return null
+	if texture is AtlasTexture:
+		var image := Image.new()
+		image = texture.get_image()
+		var image_texture := ImageTexture.new()
+		image_texture = image_texture.create_from_image(image)
+		return image_texture
+	else:
+		return texture
